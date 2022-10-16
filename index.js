f07ba8c8e628fb34e56cf10d8e488a69b6488ac7
THARAKA BOLLEGALA <tharaka94priyanjula@gmail.com>
2022-10-16 11:01:19 +0530

    Second commit

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
  */
 
 import {AppRegistry} from 'react-native';
-import Assignment from './android/src/Assignment';
 import App from './App';
 import {name as appName} from './app.json';
+import Assignment from './src/Assignment';
 
-AppRegistry.registerComponent(appName, () => );
+AppRegistry.registerComponent(appName, () => Assignment);
